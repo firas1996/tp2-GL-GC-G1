00dc4335f43326604a5f031aea9067ad5e0a3d1e
firas hamila <firas.hm.1996@gmail.com>
2024-10-30 16:17:09 +0100

    aaaa

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,3 +14,19 @@ exports.createUser = async (req, res) => {
     });
   }
 };
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: "success",
+      result: users.length,
+      data: { users },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
