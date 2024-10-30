0a4714c03a4e784e6bf7b22f2c23648cb87fe50a
firas hamila <firas.hm.1996@gmail.com>
2024-10-30 15:05:30 +0100

    aa

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
 const express = require("express");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
 
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = express();
 app.use(express.json());
+app.use("/users", userRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => {
