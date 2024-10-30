821ea75305727d202edc59594cad7f870a5cf2f9
firas hamila <firas.hm.1996@gmail.com>
2024-10-30 14:20:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`App is running on port: ${port}...`);
+});
+
+// Connect TO THe DATABASE
+
+const DB = process.env.DATABASE.replace(
+  "<db_password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then((connection) => {
+    console.log("DB connected succesfuly...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// app.get("/msg", (req, res) => {
+//   res.send("This msg from the server");
+// });
+
+const specs = JSON.parse(fs.readFileSync("./spec.json", "utf-8"));
+
+app.get("/specs", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    result: specs.length,
+    data: {
+      specs,
+    },
+  });
+});
+
+app.post("/specs", (req, res) => {
+  const id = specs[specs.length - 1].id + 1;
+  const newSpac = Object.assign({ id: id }, req.body);
+  specs.push(newSpac);
+  fs.writeFile("./spec.json", JSON.stringify(specs), (err) => {
+    res.status(201).json({
+      status: "success",
+      newSpac,
+    });
+  });
+});
